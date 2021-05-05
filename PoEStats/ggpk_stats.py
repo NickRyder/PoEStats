4bdb400c64b66a2d601c31f5080c51639328558e
Nick Ryder <nickryder@openai.com>
2021-05-04 20:45:50 -0700

    got working mod_to_ggpk prototype

--- PoEStats/ggpk_stats.py
+++ PoEStats/ggpk_stats.py
@@ -65,23 +65,24 @@ class FormatterError(Exception):
 
 @dataclass(unsafe_hash=True)
 class StatFormatter:
-    condition: Tuple[StatRange]
-    format: Tuple[StatFormat]
-    index_handlers: Tuple[List[IndexHandler]]
-    string: str
+    condition: Tuple[StatRange] = tuple()
+    format: Tuple[StatFormat] = tuple()
+    index_handlers: Tuple[Tuple[IndexHandler]] = tuple()
+    string: str = ""
 
-    def __init__(self, json):
-        self.condition = tuple(
-            [StatRange(condition) for condition in json["condition"]]
+    @classmethod
+    def load(cls, json):
+        return StatFormatter(
+            condition=tuple([StatRange(condition) for condition in json["condition"]]),
+            format=tuple([StatFormat(format) for format in json["format"]]),
+            index_handlers=tuple(
+                [
+                    tuple([IndexHandler.from_id(handler) for handler in handlers])
+                    for handlers in json["index_handlers"]
+                ]
+            ),
+            string=json["string"],
         )
-        self.format = tuple([StatFormat(format) for format in json["format"]])
-        self.index_handlers = tuple(
-            [
-                tuple([IndexHandler.from_id(handler) for handler in handlers])
-                for handlers in json["index_handlers"]
-            ]
-        )
-        self.string = json["string"]
 
     def values_to_string(self, values: Tuple[float]):
         formatted_strings = []
@@ -111,21 +112,28 @@ class StatFormatter:
 class GGPKStatTranslation:
     ids: Tuple[str]
     formatters: Tuple[StatFormatter]
+    hidden: bool = False
 
     @classmethod
     def load(cls, json: dict):
         return cls(
             ids=tuple(json["ids"]),
             formatters=tuple(
-                [StatFormatter(stat_formatter) for stat_formatter in json["English"]]
+                [
+                    StatFormatter.load(stat_formatter)
+                    for stat_formatter in json["English"]
+                ]
             ),
+            hidden=json.get("hidden", False),
         )
 
-    def translate(self, id_to_value_dict: Dict[str, int]):
+    def translate(self, id_to_value_dict: Dict[str, int]) -> Optional[str]:
+        if self.hidden:
+            return None
         values = [id_to_value_dict.get(id, 0) for id in self.ids]
 
         if all([value == 0 for value in values]):
-            raise FormatterError("No ids from this translation")
+            raise ValueError("Translating with all zero values")
 
         for formatter in self.formatters:
             try:
@@ -140,43 +148,95 @@ class GGPKStatTranslation:
         pass
 
 
+stats_with_no_translation = set()
+
+all_translatable_stats = set()
+
+
+class StatTranslations(list):
+    def __init__(self, stat_translations_json: List[dict]):
+        super().__init__([GGPKStatTranslation.load(v) for v in stat_translations_json])
+
+        self._stat_id_to_stat_translation = {}
+        for stat_translation in self:
+            for id in stat_translation.ids:
+                assert id not in self._stat_id_to_stat_translation
+                self._stat_id_to_stat_translation[id] = stat_translation
+
+    def from_id(self, id: str) -> GGPKStatTranslation:
+        if id not in self._stat_id_to_stat_translation:
+            stats_with_no_translation.add(id)
+            # if id in all_translatable_stats:
+            #     print(id)
+            #     breakpoint()
+            # assert id not in all_translatable_stats, id
+            # if id in WHITELIST:
+            return GGPKStatTranslation(
+                (id,), formatters=(StatFormatter(),), hidden=True
+            )
+        return self._stat_id_to_stat_translation[id]
+
+
 # grab both the main stat_translations and the sub_stat_translations
-stat_translations = list(
-    {GGPKStatTranslation.load(v) for v in stat_translations_json}
-    # | {
-    #     GGPKStatTranslation.load(v)
-    #     for sub_translations in sub_stat_translations.values()
-    #     for v in sub_translations
-    # }
-)
 
-stat_id_to_stat_translation: Dict[str, GGPKStatTranslation] = {}
+stat_translations = StatTranslations(stat_translations_json)
+stat_translations_atlas = StatTranslations(sub_stat_translations["atlas"])
+stat_translations_areas = StatTranslations(sub_stat_translations["areas"])
 
-for stat_translation in stat_translations:
-    for id in stat_translation.ids:
-        assert id not in stat_id_to_stat_translation
-        stat_id_to_stat_translation[id] = stat_translation
+all_translatable_stats = {
+    id for stat_translation in stat_translations_json for id in stat_translation["ids"]
+} | {
+    id
+    for _stat_translations in sub_stat_translations.values()
+    for stat_translation in _stat_translations
+    for id in stat_translation["ids"]
+}
+
+domain_to_translation = {
+    "affliction_jewel": stat_translations,
+    "delve": stat_translations_areas,
+    "flask": stat_translations,
+    "misc": stat_translations,
+    "atlas": stat_translations_atlas,
+    "crafted": stat_translations,
+    "abyss_jewel": stat_translations,
+    "watchstone": stat_translations,
+    "item": stat_translations,
+    "area": stat_translations_areas,
+}
+
+
+def translate_mod(mod: dict):
+    item_stat_translation = domain_to_translation[mod["domain"]]
+    mod_id_values = {
+        stat["id"]: stat["min"] for stat in mod["stats"] if stat["min"] != 0
+    }
+    while mod_id_values:
+        id = next(iter(mod_id_values.keys()))
+        stat_translation = item_stat_translation.from_id(id)
+        print(stat_translation.translate(mod_id_values))
+        for id in stat_translation.ids:
+            if id in mod_id_values:
+                del mod_id_values[id]
 
 
 if __name__ == "__main__":
     from RePoE import mods
-    import logging
+    from pprint import pprint
 
-    for mod in mods.values():
+    for name, mod in mods.items():
         print("")
-        mod_id_values = {stat["id"]: stat["min"] for stat in mod["stats"]}
-        lines = 0
-        while mod_id_values:
-            id = next(iter(mod_id_values.keys()))
-            if id in stat_id_to_stat_translation:
-                stat_translation = stat_id_to_stat_translation[id]
-                print(stat_translation.translate(mod_id_values))
-                lines += 1
-                for id in stat_translation.ids:
-                    if id in mod_id_values:
-                        del mod_id_values[id]
-            else:
-                logging.warn(f"Found no stat translation for {id}")
-                del mod_id_values[id]
-        if lines > 1:
-            breakpoint()
+        try:
+            if name not in ["LocalPhysicalDamageAddedAsEachElementTransformed"]:
+                translate_mod(mod)
+        except Exception as e:
+            print(name)
+            pprint(mod)
+            raise e
+
+    NotImplemented
+    # todo analyze stats_with_no_translation
+    # make sure they dont show up in sub stat translations
+
+    # later todo: make ranges display in translate
+    # later todo: allow reverse translation
--- PoEStats/translators.py
+++ PoEStats/translators.py
@@ -3,7 +3,7 @@ import re
 from typing import List
 from PoEQuery.official_api_result import Mod
 
-from PoEStats.ggpk_stats import GGPKStatTranslation, stat_translations_parsed
+from PoEStats.ggpk_stats import GGPKStatTranslation, domain_to_translation
 from PoEStats.trade_stats import stats, option_stats
 from PoEStats.re_strings import re_hashtag_pattern, re_insert_pattern
 
@@ -13,44 +13,63 @@ import logging
 # pre_cache a bunch of translation tools
 ###
 
-# remove things like (Local) (Shields) (Maps) from the end of stats
-modified_stats = {k: re.split(r" \(.+\)", v)[0] for k, v in stats.items()}
+from RePoE import mods
 
-stats_split = {}
-for stat_id, stat_string in modified_stats.items():
-    split_string = tuple(re.split(re_hashtag_pattern, stat_string))
-    # confirm that no two distinct stat_strings split into the same split_string
-    assert all(
-        [
-            modified_stats[k] == stat_string
-            for k, v in stats_split.items()
-            if v == split_string
-        ]
-    )
-    stats_split[stat_id] = split_string
 
-ggpk_stats_split = defaultdict(list)
-for parsed in stat_translations_parsed:
+def create_mod_stats_lookup():
+    stat_id_set_to_mod_names = defaultdict(list)
+    for name, mod in mods.items():
+        ids = frozenset({stat["id"] for stat in mod["stats"]})
+        stat_id_set_to_mod_names[ids].append(name)
+    return stat_id_set_to_mod_names
 
-    split_string_to_formatters = defaultdict(list)
 
-    for formatter in parsed.formatters:
-        split_string = tuple(re.split(re_insert_pattern, formatter.string))
-        # TODO: maybe dong want to split if a translator has multiple formatters
-        # that work, instead perhaps need to make a new dataclass for split matches
-        split_string_to_formatters[split_string].append(formatter)
+def create_stats_split_lookup():
+    # remove things like (Local) (Shields) (Maps) from the end of stats
+    modified_stats = {k: re.split(r" \(.+\)", v)[0] for k, v in stats.items()}
 
-    for split_string, formatters in split_string_to_formatters:
-
-        ggpk_stats_split[split_string].append(
-            GGPKStatTranslation(ids=parsed.ids, formatters=tuple(formatters)),
+    stats_split = {}
+    for stat_id, stat_string in modified_stats.items():
+        split_string = tuple(re.split(re_hashtag_pattern, stat_string))
+        # confirm that no two distinct stat_strings split into the same split_string
+        assert all(
+            [
+                modified_stats[k] == stat_string
+                for k, v in stats_split.items()
+                if v == split_string
+            ]
         )
+        stats_split[stat_id] = split_string
+    return stats_split
+
+
+def create_ggpk_stats_split_lookup():
+    ggpk_stats_split = defaultdict(list)
+
+    all_stat_translations = {
+        stat_translation
+        for stat_translations in domain_to_translation.values()
+        for stat_translation in stat_translations
+    }
+    for parsed in all_stat_translations:
+
+        for formatter in parsed.formatters:
+            split_string = tuple(re.split(re_insert_pattern, formatter.string))
+            ggpk_stats_split[split_string].append(
+                GGPKStatTranslation(ids=parsed.ids, formatters=tuple([formatter])),
+            )
+    return ggpk_stats_split
+
+
+stats_split = create_stats_split_lookup()
+ggpk_stats_split = create_ggpk_stats_split_lookup()
+stat_id_set_to_mod_names = create_mod_stats_lookup()
 
 for key, value in stats_split.items():
     if "pseudo" not in key and "veiled" not in key:
         if not value in ggpk_stats_split:
             logging.warn(
-                f"stat translation not found in ggpk : ", value, key in option_stats
+                f"stat translation not found in ggpk : {value}, {key in option_stats}"
             )
 
 
@@ -62,22 +81,49 @@ def mod_to_ggpk(
     stat "explicit.stat_3102860761" = "#% increased Movement Speed for # seconds on Throwing a Trap"
     (min, max) values for stat
     """
+    possible_mod_names = []
     hash_to_min_max_tuples = defaultdict(list)
 
     for magnitude in mod.magnitudes:
         hash_to_min_max_tuples[magnitude.hash_].append((magnitude.min_, magnitude.max_))
 
+    # for each extended mod stat...
     for hash, min_max_tuples in hash_to_min_max_tuples.items():
         stat_translations = ggpk_stats_split[stats_split[hash]]
+        breakpoint()
+        # for each possible stat translation...
         for stat_translation in {
             stat_translation
             for stat_translation in stat_translations
-            if len(stat_translation[1].ids) == len(min_max_tuples)
+            if len(stat_translation.ids) == len(min_max_tuples)
         }:
-            for formatter in stat_translation.formatter:
-                formatter
+            # find the min and max from reversing the handlers...
+            for formatter in stat_translation.formatters:
+                id_to_stat_range = {}
+                for id, index_handler, (min, max) in zip(
+                    stat_translation.ids, formatter.index_handlers, min_max_tuples
+                ):
+                    for handler in index_handler:
+                        min, max = (
+                            handler.reverse_handler(min),
+                            handler.reverse_handler(max),
+                        )
+                    id_to_stat_range[id] = (min, max)
 
-                breakpoint()
+                # check all possible mods which have the same stat ids...
+                for mod_name in stat_id_set_to_mod_names[
+                    frozenset(stat_translation.ids)
+                ]:
+                    mod = mods[mod_name]
+                    for stat in mod["stats"]:
+                        if (
+                            stat["min"] != id_to_stat_range[stat["id"]][0]
+                            or stat["max"] != id_to_stat_range[stat["id"]][1]
+                        ):
+                            break
+                    else:
+                        possible_mod_names.append(mod_name)
+    return possible_mod_names
 
 
 if __name__ == "__main__":
@@ -86,7 +132,7 @@ if __name__ == "__main__":
 
     mod = Mod(
         json.loads(
-            '{"name": "Razor-sharp", "tier": "P3", "magnitudes": [{"hash": "explicit.stat_1940865751", "min": 14, "max": 19}, {"hash": "explicit.stat_1940865751", "min": 29, "max": 35}]}'
+            '{"name": "of the Miser", "tier": "S3", "magnitudes": [{"hash": "explicit.stat_1471729472", "min": 5, "max": 7}]}'
         )
     )
-    mod_to_ggpk(mod)
+    print(mod_to_ggpk(mod))
